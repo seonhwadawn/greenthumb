eef73315a8a7f060d040a68c1135d830ce976fd2
unknown <jewel1468@gmail.com>
2022-06-27 01:32:08 +0900

    update

---
+++ greenthumb/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header">언제 물 줬지?</div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ greenthumb/src/Calender.js
@@ -0,0 +1,9 @@
+function Calender() {
+  return (
+    <div>
+      <>여기에 캘린더 넣을거임</>
+    </div>
+  );
+}
+
+export default Calender;
